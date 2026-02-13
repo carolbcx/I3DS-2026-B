b92c38738fa00c7d07b9fbed8986dfa8d250dbdb
carolbcx <carolbarbosadecastro@gmail.com>
2026-02-13 08:31:04 -0300

    adicionar componente Rodape com props e criar arquivo de estilos base

--- 03-devlinks/src/components/Rodape/Rodape.jsx
+++ 03-devlinks/src/components/Rodape/Rodape.jsx
@@ -1,9 +1,9 @@
-import React, { Children } from 'react'
 
-function Rodape() {
+
+function Rodape({children}) {
   return (
     <footer>
-          <p>Feito com por <a href="https://github.com">{Children}</a>
+          <p>Feito pela <a href="https://github.com">{children}</a>
       
     </p>
     </footer>
