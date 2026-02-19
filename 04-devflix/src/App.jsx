dcf4e8d9258387f7a93d5e3149460457d12aaa44
carolbcx <carolbarbosadecastro@gmail.com>
2026-02-19 17:06:46 -0300

    Atualizar estilos do tema e refatorar componente Rodape para incluir link dinâmico

--- 04-devflix/src/App.jsx
+++ 04-devflix/src/App.jsx
@@ -1,13 +1,33 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import logo from "./assets/devflix.png";
 import lupa from "./assets/search.svg";
 import Rodape from "./components/Rodape/Rodape";
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
+
+  //Utilizando uma CHAVE de API do arquivo .env
+  const apiKey = import.meta.env.VITE_OMDB_API_KEY;
+  const apiUrl = `https://omdbapi.com/?apikey=${apiKey}`;
+
+  //Criando a conexão com a API e trazendo informações
+  const searchMovies = async (title) => {
+    const response = await fetch(`${apiUrl}&s=${title}`);
+    const data = await response.json;
+
+    //Alimentando a variavel movies
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Batman");
+  }, []);
+
   return (
     <div id="App">
       <img
-        id="logo"
+        id="Logo"
         src={logo}
         alt="Logotipo da plataforma de streaming DEVFLIX com fundo preto e o nome em letras vermelhas destacadas"
       />
@@ -16,7 +36,13 @@ const App = () => {
         <input type="text" placeholder="Pesquise por filmes e séries..." />
         <img src={lupa} alt="Botão de ação para pesquisa!" />
       </div>
-      <Rodape>Caroline</Rodape>
+
+      <div className="container">
+        {movies.map((movie, index) =>)
+        }
+      </div>
+
+      <Rodape link={"https://github.com/carolbcx"}>carolbcx</Rodape>
     </div>
   );
 };
